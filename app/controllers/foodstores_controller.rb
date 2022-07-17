9ba85d848b490f5763eecc6e13c9df346619ce80
saiganeshnaidu <saiganeshnaiduthota@gmail.com>
2022-07-17 15:31:16 +0530

    resolved foodstore destroy problem

--- app/controllers/foodstores_controller.rb
+++ app/controllers/foodstores_controller.rb
@@ -5,11 +5,15 @@ class FoodstoresController < ApplicationController
     @foodstore = @foodcategory.foodstores.create(store_params)
     redirect_to foodcategory_path(@foodcategory)
   end
+  def show
+    @foodstore=Foodstore.find(params[:id])
+  end
   def destroy
-    @foodstore = Foodstore.find(params[:id])
+    @foodcategory = Foodcategory.find(params[:foodcategory_id])
+    @foodstore = @foodcategory.foodstores.find(params[:id])
     @foodstore.destroy
 
-    redirect_to root_path, status: :see_other
+    redirect_to foodcategory_path, status: :see_other
   end
   private
     def store_params
