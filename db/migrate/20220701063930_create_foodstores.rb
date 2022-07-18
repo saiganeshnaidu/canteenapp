352d9c753499ec90d747b032ad2998bbe9eea535
saiganeshnaidu <saiganeshnaiduthota@gmail.com>
2022-07-18 12:32:49 +0530

    resolved

--- db/migrate/20220701063930_create_foodstores.rb
+++ db/migrate/20220701063930_create_foodstores.rb
@@ -2,8 +2,6 @@ class CreateFoodstores < ActiveRecord::Migration[7.0]
   def change
     create_table :foodstores do |t|
       t.string :name
-      t.references :foodcategory, null: false, foreign_key: true
-
       t.timestamps
     end
   end
