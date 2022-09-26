18e552ad96347323cdd6c69a9b80f8f7da867c8f
saiganeshnaidu <saiganeshnaiduthota@gmail.com>
2022-09-26 10:18:26 +0530

    updated seeds file

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,6 +14,7 @@ class WelcomeController < ApplicationController
   end
 
   def admin_dashboard
+    @heading="Admin Dashboard"
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(email:"ganeshadmin@gmail.com", password:"997777@Sn", password_confirmation:"997777@Sn", usertype:"Admin")
+puts "Admin has been created"
+
+Company.create!([{name: "Kreeti Technologies"}, {name: "KT-core"}, {name: "Kreeti Pvt Lmt"}])
+puts "Companies are created"
+
+FoodCategory.create!([{name: "North Indian"}, {name: "South Indian"}, {name: "Chinese"}])
+puts "Foodcategories are created"
+
+Foodstore.create!([{name: "North spices", foodcategory_id:FoodCategory.first.id }, {name: "Street Byte", foodcategory_id:FoodCategory.first.id }])
+puts "Foodstores are created"
+
+User.create!(email: "sai@gmail.com", password: "997777@Sn", password_confirmation: "997777@Sn", usertype: "Employee")
+EmployeeProfile.create!(name: "Sai Ganesh", phone: "9014632028", company_id: Company.first.id, user_id: User.last.id, isapproved: false, isrejected:false)
+puts "created employee profile"
+
+User.create!(email: "ganesh@gmail.com", password: "997777@Sn", password_confirmation: "997777@Sn", usertype: "Chef")
+Chefprofile.create!(name: "Ganesh", phone: "9014632029", foodstore_id: Foodstore.first.id, user_id: User.last.id, isapproved: false, isrejected:false)
+puts "created chef profile"
+
+User.create!(email: "tganesh@gmail.com", password: "997777@Sn", password_confirmation: "997777@Sn", usertype: "Normal Customer")
+NormalCustomer.create!(name: "SGani", phone: "9014632028", user_id: User.last.id, isapproved: false, isrejected:false)
+puts "created normal cusomer profile"
+
+Fooditem.create!([{name: "Biryani", price: 220, description: "Authentic Chicken Biryani", foodstore_id: Foodstore.first.id}, {name: "Thali", price: 250, description: "Single server North Indian Thali", foodstore_id: Foodstore.first.id}])
+puts "Fooditems are created"
